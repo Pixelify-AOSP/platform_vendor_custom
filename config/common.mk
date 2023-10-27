f4259048644b7d5253b8fb247a93f023ad01025f
Pranav Vashi <neobuddy89@gmail.com>
2023-10-28 02:43:46 +0530

    crdroid: Build GameSpace
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- config/common.mk
+++ config/common.mk
@@ -71,6 +71,10 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.biometrics.face.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/android.hardware.biometrics.face.xml
 endif
 
+# GameSpace
+PRODUCT_PACKAGES += \
+    GameSpace
+
 # GMS
 include vendor/custom/config/pixel.mk
 
