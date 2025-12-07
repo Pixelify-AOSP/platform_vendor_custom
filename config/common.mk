9657fff968a111b5806ea46341f81f98d9f59a72
hridaya <hridaya@pixelos.net>
2025-12-07 09:50:52 +0545

    config: Drop SimpleSettingsConfig
    Change-Id: I2235ad147a3c36b4e399c61a2adf1909b428a944
    Signed-off-by: kenway214 <kenway214@outlook.com>

--- config/common.mk
+++ config/common.mk
@@ -124,10 +124,6 @@ endif
 PRODUCT_PACKAGES += \
     framework_compatibility_matrix.lineage.xml
 
-# Config
-PRODUCT_PACKAGES += \
-    SimpleSettingsConfig
-
 # Extra tools
 PRODUCT_PACKAGES += \
     bash \
