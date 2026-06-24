ffafcc2ac68cd9a7bca9b9ba79915fe81c2dc0f0
John Galt <johngaltfirstrun@gmail.com>
2026-06-24 22:50:36 +0530

    vendor: Add preinstalled package list to stop auto cloning
    Only extra adds needed, gms now ships it's own lists for this.
    
    Change-Id: I61c3cc4a35429aff5d24c001e13cef9313c03a66
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: kenway214 <kenway214@outlook.com>

--- config/common.mk
+++ config/common.mk
@@ -30,6 +30,10 @@ PRODUCT_PRODUCT_PROPERTIES += \
 endif
 endif
 
+# Cloned app exemption
+PRODUCT_COPY_FILES += \
+    vendor/custom/prebuilt/common/etc/sysconfig/preinstalled-packages-platform-ascp-product.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/sysconfig/preinstalled-packages-platform-ascp-product.xml
+
 ifeq ($(TARGET_BUILD_VARIANT),eng)
 # Disable ADB authentication
 PRODUCT_SYSTEM_EXT_PROPERTIES += ro.adb.secure=0
