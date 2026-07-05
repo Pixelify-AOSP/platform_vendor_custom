DerpFest-AOSP/android_vendor_derpfest

config: Add prop to fake encryption status
* Some banking apps check for encryption status & this prop can easily bypass it.

config/common.mk
@@ -40,6 +40,9 @@ endif
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += persist.sys.strictmode.disable=true
 endif
 
+# Fake Encryption 
+PRODUCT_SYSTEM_PROPERTIES += ro.crypto.state=encrypted
+
 # Enable Material Design 3 Expressive
 PRODUCT_PRODUCT_PROPERTIES += \
     is_expressive_design_enabled=true

Pixelify-AOSP/platform_vendor_custom

Change-Id: Iecb12f73fe14307f01ba9722711271d612559d25
Signed-off-by: kenway214 <kenway214@outlook.com>

config/common.mk
@@ -122,6 +122,9 @@ PRODUCT_PRODUCT_PROPERTIES += \
 PRODUCT_SYSTEM_EXT_PROPERTIES += \
     ro.launcher.depth.widget=true
 
+# Fake Encryption 
+PRODUCT_PRODUCT_PROPERTIES += ro.crypto.state=encrypted
+
 # Do not include art debug targets
 PRODUCT_ART_TARGET_INCLUDE_DEBUG_BUILD := false
 