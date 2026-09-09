fb29402950b63567fda1474cac1d001c98a4b80b
kenway214 <kenway214@outlook.com>
2026-09-09 15:35:30 +0530

    vendor: Drop Pixel Launcher
    Change-Id: I42f2a0097c22a262f133b0e3375ebd03be349f45
    Signed-off-by: kenway214 <kenway214@outlook.com>

--- config/pixel.mk
+++ config/pixel.mk
@@ -12,9 +12,6 @@ $(call inherit-product, vendor/pixel/prebuilts/config.mk)
 # Pixel Sounds
 $(call inherit-product, vendor/pixel/sounds/products/sounds.mk)
 
-# Pixel Launcher
-$(call inherit-product, vendor/pixel/launcher/products/launcher.mk)
-
 # Pixel ThemePicker
 $(call inherit-product, vendor/pixel/themepicker/products/themepicker.mk)
 
