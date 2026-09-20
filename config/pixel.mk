fe62580668d149b7d730749fc056dbd0ebd6a118
kenway214 <kenway214@outlook.com>
2026-09-21 04:48:43 +0530

    vendor: Remove Pixel Prebuilts product inheritance
    Change-Id: I5a2e0c2e8f9df2cbad4ad59c88eeb0846eab3e7f

--- config/pixel.mk
+++ config/pixel.mk
@@ -10,9 +10,6 @@ $(call inherit-product, vendor/pixel/clocks/products/clocks.mk)
 # Pixel GMS
 $(call inherit-product, vendor/pixel/gms/products/gms.mk)
 
-# Pixel Prebuilts
-$(call inherit-product, vendor/pixel/prebuilts/config.mk)
-
 # Pixel Sounds
 $(call inherit-product, vendor/pixel/sounds/products/sounds.mk)
 
